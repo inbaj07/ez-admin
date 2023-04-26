0f9343f221e9f91aa05019b2e97a605fabfb333f
inbaj07 <inbaj07@gmail.com>
2023-04-26 10:08:49 +0530

    eslint gitignore

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-const express = require('express');
-const app = module.exports = express();
-var bodyParser = require('body-parser');
-const path = require('path');
+const express = require('express')
+const app = module.exports = express()
+const bodyParser = require('body-parser')
+const path = require('path')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 /**
  * Set `views` directory for module
  */
 
- app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 
- /**
+/**
   * Set `view engine` to `pug`.
   */
- 
- app.set('view engine', 'pug');
+
+app.set('view engine', 'pug')
 
 /**
  * Link models with
  * mongoDB database
  */
 
- require('./lib/models')(app);
+require('./lib/models')(app)
 
- /**
+/**
  * routes application
  */
 
-require('./lib/routes')(app);
+require('./lib/routes')(app)
 
-
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 const server = app.listen(port, () => {
-    console.log(`Express server listening on port ${port}.\nEnvironment: ${process.env.NODE_ENV}`);
-});
+  console.log(`Express server listening on port ${port}.\nEnvironment: ${process.env.NODE_ENV}`)
+})
 
- app.get('/data', function (req, res) {
+app.get('/data', function (req, res) {
   res.render('index', {
-    title: 'DigiScheduler Backend API',
-  });
-});
+    title: 'DigiScheduler Backend API'
+  })
+})
